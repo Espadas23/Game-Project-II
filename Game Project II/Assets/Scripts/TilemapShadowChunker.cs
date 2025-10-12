50ed59a7238027a59e212d6f4e3b403a446cb0a2
Espadas23 <145867693+Espadas23@users.noreply.github.com>
2025-10-13 00:03:41 +0300

    1

--- Game Project II/Assets/Scripts/TilemapShadowChunker.cs
+++ Game Project II/Assets/Scripts/TilemapShadowChunker.cs
@@ -1,80 +1,3 @@
-/*using UnityEngine;
-using UnityEngine.Tilemaps;
-using UnityEngine.Rendering.Universal;
-
-[ExecuteInEditMode]
-public class TilemapShadowChunker : MonoBehaviour
-{
-    public Tilemap tilemap;
-    public int chunkSize = 32; // размер чанка
-    public CompositeShadowCaster2D composite;
-
-    [ContextMenu("Generate Shadow Chunks")]
-    public void GenerateChunks()
-    {
-        if (tilemap == null) tilemap = GetComponent<Tilemap>();
-        if (tilemap == null)
-        {
-            Debug.LogError("Tilemap not found!");
-            return;
-        }
-
-        ClearChunks();
-
-        BoundsInt bounds = tilemap.cellBounds;
-        Vector3 cellSize = tilemap.cellSize;
-
-        for (int cx = bounds.xMin; cx <= bounds.xMax; cx += chunkSize)
-        {
-            for (int cy = bounds.yMin; cy <= bounds.yMax; cy += chunkSize)
-            {
-                bool hasTile = false;
-                for (int x = 0; x < chunkSize && cx + x <= bounds.xMax; x++)
-                    for (int y = 0; y < chunkSize && cy + y <= bounds.yMax; y++)
-                        if (tilemap.GetTile(new Vector3Int(cx + x, cy + y, 0)) != null)
-                        {
-                            hasTile = true;
-                            break;
-                        }
-
-                if (!hasTile) continue;
-
-                Vector3 chunkWorldPos = tilemap.CellToWorld(new Vector3Int(cx, cy, 0));
-                chunkWorldPos += new Vector3(cellSize.x * chunkSize / 2f, cellSize.y * chunkSize / 2f, 0f);
-
-                GameObject chunkGO = new GameObject($"ShadowChunk_{cx}_{cy}");
-                chunkGO.transform.SetParent(transform, false);
-                chunkGO.transform.position = chunkWorldPos;
-
-                // PolygonCollider2D, пустой, isTrigger = true
-                var poly = chunkGO.AddComponent<PolygonCollider2D>();
-                poly.isTrigger = true;
-
-                // ShadowCaster2D
-                var sc = chunkGO.AddComponent<ShadowCaster2D>();
-                sc.castsShadows = true;
-                sc.selfShadows = false;
-#if UNITY_2022_1_OR_NEWER
-                sc.alphaCutoff = 1f;
-#endif
-            }
-        }
-
-        if (composite == null)
-            composite = GetComponent<CompositeShadowCaster2D>() ?? gameObject.AddComponent<CompositeShadowCaster2D>();
-    }
-
-    [ContextMenu("Clear Shadow Chunks")]
-    public void ClearChunks()
-    {
-        for (int i = transform.childCount - 1; i >= 0; i--)
-        {
-            var c = transform.GetChild(i);
-            if (c.name.StartsWith("ShadowChunk"))
-                DestroyImmediate(c.gameObject);
-        }
-    }
-}*/
 
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -184,6 +107,18 @@ public class TilemapShadowChunker : MonoBehaviour
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
